0135cd6306516a0569191151421955c3a756d4a7
scarletjasmine <scarletjasmine@proton.me>
2025-06-18 23:24:06 +0000

    ,,,

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,4 +1,33 @@
 import { Hono } from "hono";
+import { createRequestHandler } from "react-router";
+
+const app = new Hono();
+
+// Add more routes here
+
+app.get("*", (c) => {
+  const requestHandler = createRequestHandler(
+    () => import("virtual:react-router/server-build"),
+    import.meta.env.MODE,
+  );
+
+  return requestHandler(c.req.raw, {
+    cloudflare: { env: c.env, ctx: c.executionCtx },
+  });
+});
+
+export default app;
+
+
+
+
+
+
+/** 
+
+
+
+import { Hono } from "hono";
 import { OpenAI } from 'openai';
 
 
@@ -19,3 +48,4 @@ export default {
   }
 
 }
+*/
