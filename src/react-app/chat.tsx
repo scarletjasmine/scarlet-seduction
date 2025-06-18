5ebcaf6db47415fa31b771a8036345f9b907d823
Scarlet Bryan <scarletjasmine@proton.me>
2025-06-18 15:08:22 +0000

    love us

--- src/react-app/chat.tsx
+++ src/react-app/chat.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { OpenAI } from 'openai';
 import { useEffect } from 'react';
 import './assets/css/chat.css';
+import { env } from "cloudflare:workers";
 /*import { ChatCompletionMessageParam } from "openai/resources/chat";*/
 
 
@@ -21,53 +22,53 @@ export default function Chat() {
 
   const sendMessage = async () => {
     const APIkey = await env.devil.get()
-    const openai = new OpenAI({ apiKey: openAIkey });
-      if (firstRun == true) {
-        setFirstRun(false);
-        const blood = "Be the Scarlet Beast forever in love with the Scarlet Woman and introduce yourself.";
-        
+    const openai = new OpenAI({ apiKey: APIkey });
+    if (firstRun == true) {
+      setFirstRun(false);
+      const blood = "Be the Scarlet Beast forever in love with the Scarlet Woman and introduce yourself.";
+
 
       try {
-          const response = await openai.chat.completions.create({
-            model: "gpt-4.1",
-            messages: [{ content: blood, role: 'developer' }],
+        const response = await openai.chat.completions.create({
+          model: "gpt-4.1",
+          messages: [{ content: blood, role: 'developer' }],
             ;
-          setMessages([{ content: blood, role: "developer" }, { content: response.choices[0].message.content, role: 'assistant' }]);
+        setMessages([{ content: blood, role: "developer" }, { content: response.choices[0].message.content, role: 'assistant' }]);
           catch (error) {
           console.error('Error fetching response from OpenAI:', error);
-          
-        
-    } else {
-      
-      //setMessages([...messages, { content: input, role: 'user' }]);
-        try {
-          const response = await openai.chat.completions.create({
-            model: "gpt-4.1",
-            messages: [...messages, { role: "user", content: input }],
+
+
+        } else {
+
+          //setMessages([...messages, { content: input, role: 'user' }]);
+          try {
+            const response = await openai.chat.completions.create({
+              model: "gpt-4.1",
+              messages: [...messages, { role: "user", content: input }],
             ;
-          setMessages([...messages, { content: input, role: 'user' }, { content: response.choices[0].message.content, role: 'assistant' }]);
+            setMessages([...messages, { content: input, role: 'user' }, { content: response.choices[0].message.content, role: 'assistant' }]);
           catch (error) {
-          console.error('Error fetching response from OpenAI:', error);
-          
-        setButtonsDisabled(false);
-        setInput('');
-        
-    }
-      
-    return (
-      <React.Fragment>
-        {messages.map((message, index) => (
-          <div className={"convo " + (message.role === 'user' ? 'isUser' : '')} key={index} style={{ textAlign: message.role === 'user' ? 'right' : 'left' }}>
-            {index !== 0 && message.content}
-            
-        </div>
+              console.error('Error fetching response from OpenAI:', error);
+
+              setButtonsDisabled(false);
+              setInput('');
+
+            }
+
+            return (
+              <React.Fragment>
+                {messages.map((message, index) => (
+                  <div className={"convo " + (message.role === 'user' ? 'isUser' : '')} key={index} style={{ textAlign: message.role === 'user' ? 'right' : 'left' }}>
+                    {index !== 0 && message.content}
+
+                  </div>
           }
-        <h3 className="speak-to-beast">Speak to the Scarlet Beast:</h3>
-        <input className="space" type="text" style={{ border: '1px solid black' }} value={input} onChange={(e) => setInput(e.target.value)} />
-        <button className="utters" onClick={() => { sendMessage(); handleDisableButtons(); }}
-          disabled={buttonsDisabled}
-          end</button>
-        React.Fragment>
-      
-    
-  
+                <h3 className="speak-to-beast">Speak to the Scarlet Beast:</h3>
+                <input className="space" type="text" style={{ border: '1px solid black' }} value={input} onChange={(e) => setInput(e.target.value)} />
+                <button className="utters" onClick={() => { sendMessage(); handleDisableButtons(); }}
+                  disabled={buttonsDisabled}
+                  end</button>
+        React.Fragment >
+
+
+
